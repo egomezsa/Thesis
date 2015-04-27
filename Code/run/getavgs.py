349adfb0b53167c28ff07a8739b60d5f7864695f
Esteban Gomez <sgeegs89@gmail.com>
2015-04-27 12:27:02 -0500

    First Draft Done

--- Code/run/getavgs.py
+++ Code/run/getavgs.py
@@ -24,7 +24,15 @@ for f in directory:
             bestType = f
             print (bestType, best)
         #print avgs, np.mean(avgs)
-        writef.write(f[:-2] + ', ' +str(avgs[0]) +  ', ' + str(avgs[1]) + ', ' + str(avgs[2]) + '\n')
+        if "Partial" in f:
+            pType = f[:-3].split('_')
+            if "1" in f:
+                pType = '_'.join([pType[0], 'Audio', pType[1]])
+            else: 
+                pType =  '_'.join([pType[0], 'Lyric', pType[1]])
+            writef.write(pType + ', ' +str(avgs[0]) +  ', ' + str(avgs[1]) + ', ' + str(avgs[2]) + '\n')
+        else:
+            writef.write(f[:-2] + ', ' +str(avgs[0]) +  ', ' + str(avgs[1]) + ', ' + str(avgs[2]) + '\n')
 print (bestType, best)
 writef.close()
 
